9bbe04bea40a4412e534c742d9f4ce18200d54c9
itsu1140 <n.itsuki1140@gmail.com>
2025-11-06 14:33:17 +0900

    update files about convert abc

--- work1/convert_abc.py
+++ work1/convert_abc.py
@@ -4,12 +4,21 @@ from pathlib import Path
 from music21 import converter
 
 
-def abc2midi(abc_path: Path):
+def abc2midi(abc_path: Path, midi_path: Path):
+    """Generate MIDI from .abc"""
     abc = converter.parse(abc_path)
-    abc.write("midi", fp="abc_midi.mid")
+    abc.write("midi", fp=midi_path)
+
+
+def midi2wav(midi_path: Path):
+    """Generate WAV from MIDI"""
+    # it doesn't work
+    wav_path = "abc.wav"
+    subprocess.run(["timidity", midi_path.name, "-Ow", "-o", wav_path], check=True)
 
 
 def abc2pdf(abc_path: Path):
+    """Generate music sheet"""
     abcps: str = "abc.ps"
     sheet: str = "sheet.pdf"
     subprocess.run(["abcm2ps", str(abc_path), "-O", abcps], check=True)
@@ -18,7 +27,9 @@ def abc2pdf(abc_path: Path):
 
 def main():
     abc_path: Path = Path("./data") / "sheet_sample.abc"
-    abc2midi(abc_path)
+    midi_path: Path = Path("abc.mid")
+    abc2midi(abc_path, midi_path)
+    midi2wav(midi_path)
     abc2pdf(abc_path)
 
 
