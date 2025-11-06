111e46d55865edc39177e6d69cbb25a2f53253ad
itsu1140 <n.itsuki1140@gmail.com>
2025-11-06 13:26:03 +0900

    remove extra install

--- work1/build.sh
+++ work1/build.sh
@@ -1,25 +1,15 @@
-#!/bin/bash
-# abcmidi install
-git clone https://github.com/sshlien/abcmidi.git
-cd abcmidi
-make abc2midi
-echo "export PATH=\"$(pwd):\$PATH\"" >> ~/.bashrc
-
 # abcm2ps install
 cd ..
 git clone https://github.com/lewdlime/abcm2ps.git
 cd abcm2ps
-./configure
-make
+./configure && make
 echo "export PATH=\"$(pwd):\$PATH\"" >> ~/.bashrc
 
 # timidity install
 cd ..
 git clone https://github.com/geofft/timidity.git
 cd timidity
-./configure
-make
-make install
+./configure && make && make install
 cd timidity
 echo "export PATH=\"$(pwd):\$PATH\"" >> ~/.bashrc
 
